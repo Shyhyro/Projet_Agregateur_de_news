cd84517348f536d7a5dc87a164a10ebcb276743f
Shyhyro <Bosquet.laurie@outlook.com>
2022-02-11 10:07:25 +0100

    gvhbijnoùk,jbhvugoycfitxd

--- src/Controller/NewsapiController.php
+++ src/Controller/NewsapiController.php
@@ -2,15 +2,11 @@
 
 namespace App\Controller;
 
-use jcobhams\NewsApi\NewsApi;
-use NewsApiTest;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/newsapi', name: 'articles_')]
-class NewsapiController extends AbstractController
+class NewsapiController
 {
     /**
      * List available articles.
@@ -25,13 +21,8 @@ class NewsapiController extends AbstractController
         $data = file_get_contents($json);
         $obj = json_decode($data);
 
-        $response2 = $obj->articles;
+        $response2 = $obj->articles[0]->author;
 
-        $newsapijson = json_decode(json_encode('https://newsapi.org/v2/top-headlines?q=general&apiKey=e4497f15dc6349dc91006f4a337f9bff'));
-
-        $newsapi = json_decode($newsapijson);
-
-        $response = $newsapi->status;
 
         return new Response($response2);
     }
